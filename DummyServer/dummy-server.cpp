322f35613c282fb0d7db1beec0b770b67c004da0
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2024-07-23 16:40:55 +0200

    fixup: Made the c++ dummy server work reliably. Corrected the usleep timer to take delays in microseconds.

--- DummyServer/dummy-server.cpp
+++ DummyServer/dummy-server.cpp
@@ -3,6 +3,9 @@
 #include <termios.h>
 #include <iostream>
 #include <cstring>
+#include <sstream>
+#include <vector>
+#include <string>
 
 //First start:
 //socat -d -d pty,raw,echo=0,link=/tmp/virtual-tty pty,raw,echo=0,link=/tmp/virtual-tty-back
@@ -11,8 +14,8 @@ class SerialPort {
     int fd;
     public:
     SerialPort(const char* device) {
-        fd = open(device, O_RDWR ); //from fcntl
-        //fd = open(device, O_RDWR | O_NOCTTY); //from fcntl
+        fd = open(device, O_RDWR | O_NOCTTY); //from fcntl
+        //O_NOCTTY is questionalbe
         if (fd == -1) {
             std::cerr << "Unable to open device " << device << std::endl;
             exit(-1); //stdlib
@@ -27,19 +30,25 @@ class SerialPort {
            // Set Baud Rate
         cfsetospeed(&tty, (speed_t)B9600);//from termios
         cfsetispeed(&tty, (speed_t)B9600);//from termios
-                                          //
-        tty.c_cflag     &=  ~PARENB;            // Make 8n1
-        tty.c_cflag     &=  ~CSTOPB;
-        tty.c_cflag     &=  ~CSIZE;
-        tty.c_cflag     |=  CS8; 
+        //These can in princiapl fail, and return ints. 
+        //If those ints are <0, then failure.
+        //see https://www.man7.org/linux/man-pages/man3/termios.3.htmlL
+        //serial_port_settings.c_cflag &= ~PARENB;: This line disables parity generation and detection1.
 
-        tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+        tty.c_cflag     &=  ~PARENB; //disable parity bit generaton and detection
+        tty.c_cflag     &=  ~CSTOPB; //This line sets the number of stop bits to one. If CSTOPB were set, two stop bits would be used1.
+
+        tty.c_cflag     &=  ~CSIZE; //clear the character size mask
+        tty.c_cflag     |=  CS8;  //set character size to 8 bits
+
+        tty.c_cflag     &=  ~CRTSCTS;           // disable hardware flow control
         tty.c_cc[VMIN]   =  0;                  // 1: read doesn't block
-        tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
-        tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+        //VMIN defines the minimum number of characters to read
 
-        // Make raw
-        cfmakeraw(&tty);
+        tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+        tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ (the CREAD receiver) & ignore modem ctrl lines
+
+        cfmakeraw(&tty);// Make raw
 
         // Flush Port, then applies attributes
         tcflush(fd, TCIFLUSH);
@@ -55,7 +64,12 @@ class SerialPort {
     }
 
     void send(const std::string& str) {
-        char junk = write(fd, str.c_str(), str.size()); //from unistd
+        int bytesWritten = write(fd, str.c_str(), str.size()); //from unistd
+
+        if (bytesWritten != static_cast<ssize_t>(str.size())) {
+            std::cerr << "Incomplete write" << std::endl;
+        }
+
     }
 
     std::string receive() {
@@ -66,19 +80,38 @@ class SerialPort {
     }
 };
 
+std::vector<std::string> splitString(const std::string& input, char delimiter) {
+    std::vector<std::string> result;
+    std::stringstream ss(input);
+    std::string item;
+    
+    // Split the string based on the delimiter
+    while (std::getline(ss, item, delimiter)) {
+        result.push_back(item);
+    }
+    
+    return result;
+}
+
 void dummyServer() {
     std::cout<<"make port"<<std::endl;
     SerialPort sp("/tmp/virtual-tty-back");
 
-    int i = 0;
+    //int i = 0;
     std::cout<<"enter while"<<std::endl;
     while(true){
         std::string data = sp.receive();
-        usleep(30); //sleep for 30ms
-        std::cout<<"rx'ed "<<data<<std::endl;
-        sp.send(data);
 
-        if(++i > 10) break;
+        usleep(50*1000L); //sleep for 50ms
+
+        std::vector<std::string> lines = splitString(data, ';');
+        for(const std::string& dat : lines){
+            std::cout<<"rx'ed "<<dat<<std::endl;
+            sp.send(dat);
+            usleep(50*1000L); //sleep for 50ms
+        }
+
+        //if(++i > 10) break;
     }
 
     std::cout<<"end"<<std::endl;
