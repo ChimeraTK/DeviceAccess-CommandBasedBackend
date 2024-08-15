b07d23fd019aded9d416cf39c6641529991a0293
Martin Killenberg <martin.killenberg@desy.de>
2024-08-15 18:22:31 +0200

    feat: test registers in DummyServer

--- tests/DummyServer.cpp
+++ tests/DummyServer.cpp
@@ -14,31 +14,124 @@
 // socat -d -d pty,raw,echo=0,link=/tmp/virtual-tty pty,raw,echo=0,link=/tmp/virtual-tty-back
 
 void DummyServer::mainLoop() {
-  static const bool debug = true;
-  if(debug) std::cout << "echoing port /tmp/virtual-tty-back" << std::endl; // DEBUG
+  if(_debug) std::cout << "echoing port /tmp/virtual-tty-back" << std::endl; // DEBUG
 
   std::string data;
   uint64_t nIter = 0;
   while(true) {
-    if(debug) std::cout << "dummy-server is patiently listening (" << nIter++ << ")..." << std::endl; // DEBUG
+    if(_debug) std::cout << "dummy-server is patiently listening (" << nIter++ << ")..." << std::endl; // DEBUG
     data = serialPort.readline();
 
-    if(debug) std::cout << "rx'ed \"" << replaceNewLines(data) << "\"" << std::endl; // DEBUG
+    if(_debug) std::cout << "rx'ed \"" << replaceNewLines(data) << "\"" << std::endl; // DEBUG
 
     if(data.find("send") == 0) { // A fake command e.g. "send 4" to test multiline readback //DEBUG section
+      if(data.size() < 6) {
+        serialPort.send("12345 Syntax error: send command needs an argument");
+        continue;
+      }
+      if(data[4] != ' ') {
+        serialPort.send("12345 Syntax error: unknown command: " + data);
+        continue;
+      }
       int n = std::atoi(data.substr(5).c_str());
       for(int i = 0; i < n; i++) {
         std::string dat = "reply " + std::to_string(i);
-        if(debug) std::cout << "tx'ing \"" << replaceNewLines(dat) << "\"" << std::endl; // DEBUG
+        if(_debug) std::cout << "tx'ing \"" << replaceNewLines(dat) << "\"" << std::endl; // DEBUG
         serialPort.send(dat);
+      }
+    }
+    else if(data == "*CLS") {
+      if(_debug) {
+        std::cout << "Received debug clear command" << std::endl;
+      }
+    }
+    else if(data == "*CLS") {
+      if(_debug) {
+        std::cout << "Received debug clear command" << std::endl;
+      }
+    }
+    else if(data == "*IDN?") {
+      serialPort.send("Dummy server for command based serial backend.");
+    }
+    else if(data == "SAI?") {
+      serialPort.send("AXIS_1");
+      serialPort.send("AXIS_2");
+    }
+    else if(data.find("ACC AXIS_1 ") == 0) {
+      setAcc(0, data);
+    }
+    else if(data.find("ACC AXIS_2 ") == 0) {
+      setAcc(1, data);
+    }
+    else if(data == "ACC?") {
+      serialPort.send("AXIS_1=" + std::to_string(acc[0]));
+      serialPort.send("AXIS_2=" + std::to_string(acc[1]));
+    }
+    else if(data == "ACC? AXIS1") {
+      serialPort.send("AXIS_1=" + std::to_string(acc[0]));
+    }
+    else if(data == "ACC? AXIS2") {
+      serialPort.send("AXIS_2=" + std::to_string(acc[1]));
+    }
+    else if(data.find("SOUR:FREQ:CW ") == 0) {
+      if(data.size() < 14) {
+        serialPort.send("12345 Syntax error: SOUR:FREQ:CW needs an argument");
+        continue;
+      }
+      try {
+        cwFrequency = std::stol(data.substr(13));
+        if(_debug) {
+          std::cout << "Setting cwFrequency to " << cwFrequency << std::endl;
+        }
+      }
+      catch(...) {
+        serialPort.send("12345 Syntax error in argument: " + data.substr(13));
+      }
+    }
+    else if(data == "SOUR:FREQ:CW?") {
+      serialPort.send(std::to_string(cwFrequency));
+    }
+    else if(data.find("CALC1:DATA:TRAC? ") == 0) {
+      // found the only right trace in this simple dummy
+      if(data.find("CALC1:DATA:TRAC? 'myTrace'") == 0) {
+        if(data.find("CALC1:DATA:TRAC? 'myTrace' SDAT") == 0) {
+          std::string out;
+          for(auto& val : trace) {
+            out += std::to_string(val) + ",";
+          }
+          out.pop_back();
+          serialPort.send(out);
+        }
+        else {
+          serialPort.send("error: unknow data format");
+        }
+      }
+      else {
+        serialPort.send("error: unknown trace");
       }
     }
     else {
       std::vector<std::string> lines = parseStr(data, ';');
       for(const std::string& dat : lines) {
-        if(debug) std::cout << "tx'ing \"" << replaceNewLines(dat) << "\"" << std::endl; // DEBUG
+        if(_debug) std::cout << "tx'ing \"" << replaceNewLines(dat) << "\"" << std::endl; // DEBUG
         serialPort.send(dat);
       }
     } // end else
   }
-} // end mainLoop
+}
+
+void DummyServer::setAcc(size_t i, std::string data) {
+  if(data.size() < 12) {
+    serialPort.send("12345 Syntax error: ACC AXIS_1 needs an argument");
+    return;
+  }
+  try {
+    acc[i] = std::stof(data.substr(11));
+    if(_debug) {
+      std::cout << "Setting acc[" << i << "] to " << acc[i] << std::endl;
+    }
+  }
+  catch(...) {
+    serialPort.send("12345 Syntax error in argument: " + data.substr(11));
+  }
+}
--- tests/DummyServer.h
+++ tests/DummyServer.h
@@ -10,7 +10,15 @@ class DummyServer {
   ~DummyServer(){};
 
   void mainLoop();
+  float acc[2]{0.2, 0.3};
+  float mov[2]{1.2, 1.3};
+  uint64_t cwFrequency{1300000000};
+  float trace[10]{0., 1., 4., 9., 16., 25., 36., 49., 64., 81.};
 
  protected:
   SerialPort serialPort{"/tmp/virtual-tty-back"};
+
+  void setAcc(size_t i, std::string data);
+
+  bool _debug{true};
 };
