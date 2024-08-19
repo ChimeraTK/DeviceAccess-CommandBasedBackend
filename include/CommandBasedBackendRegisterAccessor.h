27391152215f1f6e41c3244fe9640caaff7660ac
Martin Killenberg <martin.killenberg@desy.de>
2024-08-19 14:38:16 +0200

    fix: coding style

--- include/CommandBasedBackendRegisterAccessor.h
+++ include/CommandBasedBackendRegisterAccessor.h
@@ -7,24 +7,24 @@
 //#include "ForwardDeclarations.h"
 #include "AccessMode.h"
 #include "BackendRegisterInfoBase.h"
+#include "CommandHandler.h"
 #include "DataDescriptor.h"
 #include "IEEE754_SingleConverter.h"
 #include "NDRegisterAccessor.h"
 #include "NumericAddressedLowLevelTransferElement.h"
-#include <mutex>
 
-#include "CommandHandler.h"
+#include <mutex>
 
 //#include <ChimeraTK/cppext/finally.hpp>
 //
 
 namespace ChimeraTK {
-  template<typename UserType>//, typename DataConverterType>
+  template<typename UserType> //, typename DataConverterType>
   class CommandBasedBackendRegisterAccessor;
 
-/**
-Holds info all about the command you're sending, but about the device you're sending it to.
- */
+  /**
+  Holds info all about the command you're sending, but about the device you're sending it to.
+   */
   class CommandBasedBackendRegisterInfo : public BackendRegisterInfoBase {
    public:
     CommandBasedBackendRegisterInfo() {} // end constructor
@@ -88,8 +88,8 @@ Holds info all about the command you're sending, but about the device you're sen
     AccessModeFlags _accessModeFlags;
     RegisterPath _registerPath;
 
-   /**The SCPI base command.
-    */
+    /**The SCPI base command.
+     */
     std::string _commandName;
 
     const DataDescriptor _dataDescriptor;
@@ -99,125 +99,123 @@ Holds info all about the command you're sending, but about the device you're sen
   // backend: inheriting from DeviceBackendImpl -- mostly implementing virtual things from DeviceBackend.
   // as you put the backendRegister
 
-class CommandBasedBackend : public DeviceBackendImpl {
-  //***************** Virtual items from DeviceBackend ******************
-  public:
-   CommandBasedBackend(std::string serialDevice): _device(serialDevice) {
-    //Creates the CommandBasedRegisterAccessor
-   }
+  class CommandBasedBackend : public DeviceBackendImpl {
+    //***************** Virtual items from DeviceBackend ******************
+   public:
+    CommandBasedBackend(std::string serialDevice) : _device(serialDevice) {
+      // Creates the CommandBasedRegisterAccessor
+    }
 
-   ~CommandBasedBackend() override = default;
+    ~CommandBasedBackend() override = default;
 
-   /** Open the device */
-   void open() {
-    //Martin: instantiate CommandHandler here.
-    //make it a unique pointer
-    //here set make_unique. if it throws, catch ChimeraTK::runtime_error and setException. 
-    //else its ok. setOpenAndClearException
+    /** Open the device */
+    void open() {
+      // Martin: instantiate CommandHandler here.
+      // make it a unique pointer
+      // here set make_unique. if it throws, catch ChimeraTK::runtime_error and setException.
+      // else its ok. setOpenAndClearException
 
-     // TODO open the device.
-     if(ok){
-       setOpenedAndClearException();
-     } else {
-      setException("Error opening CommandBased Backend for "+_device)
-     }
-     //Backends should call this function at the end of a (successful) open() call
-   }
+      // TODO open the device.
+      if(ok) {
+        setOpenedAndClearException();
+      }
+      else {
+        setException("Error opening CommandBased Backend for " + _device)
+      }
+      // Backends should call this function at the end of a (successful) open() call
+    }
 
-  /** Close the device */
-  void close() override {
-    //TODO close the device.
-    _opened.store(false);
-  }
+    /** Close the device */
+    void close() override {
+      // TODO close the device.
+      _opened.store(false);
+    }
 
-  std::string sendCommand(std::string cmd){
-    _mux.lock();
-    std::string result = _dev->sendCommand(cmd);
-    _mux.unlock();
-    return result;
-  }
+    std::string sendCommand(std::string cmd) {
+      _mux.lock();
+      std::string result = _dev->sendCommand(cmd);
+      _mux.unlock();
+      return result;
+    }
 
-  std::vector<std::string> sendCommand(std::string cmd, const size_t nLinesExpected){
-    _mux.lock();
-    std::vector<std::string> result = _dev->sendCommand(cmd, nLinesExpected);
-    _mux.unlock();
-    return result;
-  }
+    std::vector<std::string> sendCommand(std::string cmd, const size_t nLinesExpected) {
+      _mux.lock();
+      std::vector<std::string> result = _dev->sendCommand(cmd, nLinesExpected);
+      _mux.unlock();
+      return result;
+    }
 
- /** Get a NDRegisterAccessor object from the register name. */
-  template<typename UserType>
-  boost::shared_ptr<CommandBasedBackendRegisterAccessor<UserType>> getRegisterAccessor(
-      CommandBasedBackendRegisterInfo& registerInfo, const RegisterPath& registerPathName,
-      AccessModeFlags flags) override {
+    /** Get a NDRegisterAccessor object from the register name. */
+    template<typename UserType>
+    boost::shared_ptr<CommandBasedBackendRegisterAccessor<UserType>> getRegisterAccessor(
+        CommandBasedBackendRegisterInfo& registerInfo, const RegisterPath& registerPathName,
+        AccessModeFlags flags) override {
+      return boost::shared_ptr<CommandBasedBackendRegisterAccessor<UserType>>(
+          new CommandBasedBackendRegisterAccessor<UserType>(
+              DeviceBackend::shared_from_this(), registerInfo registerPathName, flags));
+    }
 
-    return boost::shared_ptr<CommandBasedBackendRegisterAccessor<UserType>>(
-        new CommandBasedBackendRegisterAccessor<UserType>(
-            DeviceBackend::shared_from_this(), registerInfo registerPathName, flags));
-  }
+    /**
+     *  Return the register catalogue with detailed information on all registers.
+     */
+    RegisterCatalogue getRegisterCatalogue() const override { // TODO
+      // return RegisterCatalogue(_registerMap.clone()); //for now return an empty RegisterCatalog
+    }
 
-  /**
-   *  Return the register catalogue with detailed information on all registers.
-   */
-  RegisterCatalogue getRegisterCatalogue() const override { //TODO
-    //return RegisterCatalogue(_registerMap.clone()); //for now return an empty RegisterCatalog
-  }
+    /**
+     *  Return the device metadata catalogue
+     */
+    MetadataCatalogue getMetadataCatalogue() const override { // TODO
+      // return _metadataCatalogue; //TODO: for now return an empty metadata catalog
+    }
 
-  /**
-   *  Return the device metadata catalogue
-   */
-  MetadataCatalogue getMetadataCatalogue() const override {//TODO
-    //return _metadataCatalogue; //TODO: for now return an empty metadata catalog
-  }
+    /** Get a NDRegisterAccessor object from the register name. */
+    /*template<typename UserType>
+    boost::shared_ptr<NDRegisterAccessor<UserType>> getRegisterAccessor(
+        const RegisterPath& registerPathName, size_t numberOfWords, size_t wordOffsetInRegister, AccessModeFlags flags);
 
-  /** Get a NDRegisterAccessor object from the register name. */
-  /*template<typename UserType>
-  boost::shared_ptr<NDRegisterAccessor<UserType>> getRegisterAccessor(
-      const RegisterPath& registerPathName, size_t numberOfWords, size_t wordOffsetInRegister, AccessModeFlags flags);
+    DEFINE_VIRTUAL_FUNCTION_TEMPLATE_VTABLE(getRegisterAccessor_impl,
+        boost::shared_ptr<NDRegisterAccessor<T>>(const RegisterPath&, size_t, size_t, AccessModeFlags));*/
 
-  DEFINE_VIRTUAL_FUNCTION_TEMPLATE_VTABLE(getRegisterAccessor_impl,
-      boost::shared_ptr<NDRegisterAccessor<T>>(const RegisterPath&, size_t, size_t, AccessModeFlags));*/
+    /** Return a device information string containing hardware details like the
+     * firmware version number or the slot number used by the board. The format
+     * and contained information of this string is completely backend
+     * implementation dependent, so the string may only be printed to the user as
+     * an informational output. Do not try to parse this string or extract
+     * information from it programmatically. */
+    std::string readDeviceInfo() override {
+      return "Device: " + _device + " timeout: " + std::string(_timeoutInMilliseconds);
+    }
 
-  /** Return a device information string containing hardware details like the
-   * firmware version number or the slot number used by the board. The format
-   * and contained information of this string is completely backend
-   * implementation dependent, so the string may only be printed to the user as
-   * an informational output. Do not try to parse this string or extract
-   * information from it programmatically. */
-  std::string readDeviceInfo() override {
-    return "Device: "+_device+" timeout: "+std::string(_timeoutInMilliseconds);
-  }
+    void activateAsyncRead() noexcept override {} // Martin: leave empty.
 
-  void activateAsyncRead() noexcept override {} //Martin: leave empty.
-
-  //--------------------------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------------------------
     /*virtual void setExceptionImpl() noexcept {
       _asyncIsActive = false;//TODO done in NumericAddressedBackend
     } */
 
-    protected:
+   protected:
     std::string _device;
     ulong _timeoutInMilliseconds;
 
     /// mutex for protecting ordered port access
     std::mutex _mux;
     unique_ptr<CommandHandler> _dev;
-};
+  };
   /********************************************************************************************************************/
 
   /**
    * Implementation of the NDRegisterAccessor for CommandBasedBackend for scalar and 1D registers.
    */
-  template<typename UserType>//, DataConverterType>
+  template<typename UserType> //, DataConverterType>
   class CommandBasedBackendRegisterAccessor : public NDRegisterAccessor<UserType> {
    public:
     CommandBasedBackendRegisterAccessor(const boost::shared_ptr<DeviceBackend>& dev,
-        CommandBasedBackendRegisterInfo& registerInfo,
-        const RegisterPath& registerPathName,
-        AccessModeFlags flags)
-    : NDRegisterAccessor<UserType>(registerPathName, flags), 
-      _registerInfo(registerInfo), 
+        CommandBasedBackendRegisterInfo& registerInfo, const RegisterPath& registerPathName, AccessModeFlags flags)
+    : NDRegisterAccessor<UserType>(registerPathName, flags), _registerInfo(registerInfo),
       //_dataConverter(registerPathName), //TODO what is this?
-      _dev(boost::dynamic_pointer_cast<CommandBasedBackend>( dev)) { //_dev(boost::dynamic_pointer_cast<NumericAddressedBackend>(dev)) {
+      _dev(boost::dynamic_pointer_cast<CommandBasedBackend>(
+          dev)) { //_dev(boost::dynamic_pointer_cast<NumericAddressedBackend>(dev)) {
 
       // allocated the buffers
       NDRegisterAccessor<UserType>::buffer_2D.resize(_registerInfo.dimension);
@@ -237,7 +235,7 @@ class CommandBasedBackend : public DeviceBackendImpl {
 
       // obtain register information
       //_registerInfo = _dev->getRegisterInfo(registerPathName);
-      if(_registerInfo.channels <= 0 or _registerInfo.nElements <= 0){
+      if(_registerInfo.channels <= 0 or _registerInfo.nElements <= 0) {
         throw ChimeraTK::logic_error;
       }
 
@@ -323,8 +321,7 @@ class CommandBasedBackend : public DeviceBackendImpl {
 
     [[nodiscard]] bool isWriteable() const override { return _registerInfo.isWriteable(); }
 
-
-    /*void setExceptionBackend(boost::shared_ptr<DeviceBackend> exceptionBackend) override { 
+    /*void setExceptionBackend(boost::shared_ptr<DeviceBackend> exceptionBackend) override {
       _exceptionBackend = std::move(exceptionBackend);
     }*/     //TODO is it ok to just use the parent class implementation here? It looks perfectly good.
 
@@ -342,9 +339,8 @@ class CommandBasedBackend : public DeviceBackendImpl {
      * abstraction!
      */
     std::vector<boost::shared_ptr<TransferElement>> getHardwareAccessingElements() override {
-          return {TransferElement::shared_from_this()}; //Returns a shared pointer to `this`
+      return {TransferElement::shared_from_this()}; // Returns a shared pointer to `this`
     }
-
 
     /**
      *  Obtain the full list of TransferElements internally used by this
@@ -366,20 +362,21 @@ class CommandBasedBackend : public DeviceBackendImpl {
      * abstraction!
      */
     std::list<boost::shared_ptr<TransferElement>> getInternalElements() override {
-      //return and empty list.
+      // return and empty list.
       return std::list<boost::shared_ptr<TransferElement>>();
     }
 
+    virtual void replaceTransferElement([[maybe_unused]] boost::shared_ptr<TransferElement> newElement) {
+    } // mayReplaceTransferElement = false, so this is just empty.
 
-    virtual void replaceTransferElement([[maybe_unused]] boost::shared_ptr<TransferElement> newElement) {} //mayReplaceTransferElement = false, so this is just empty.
-
-        /** Create a CopyRegisterDecorator of the right type decorating this
+    /** Create a CopyRegisterDecorator of the right type decorating this
      * TransferElement. This is used by
      *  TransferElementAbstractor::replaceTransferElement() to decouple two
      * accessors which are replaced on the abstractor level. */
-    virtual boost::shared_ptr<TransferElement> makeCopyRegisterDecorator() = 0; //look at DOOCS backend to see how it's done. TODO
+    virtual boost::shared_ptr<TransferElement>
+        makeCopyRegisterDecorator() = 0; // look at DOOCS backend to see how it's done. TODO
 
-  /*--------------------------------------------------------------------------------------------------------------------------------*/
+    /*--------------------------------------------------------------------------------------------------------------------------------*/
    protected:
     /** Address, size and fixed-point representation information of the register
      * from the map file */
@@ -387,27 +384,28 @@ class CommandBasedBackend : public DeviceBackendImpl {
     CommandBasedBackendRegisterInfo& _registerInfo;
 
     /** raw accessor */
-    //boost::shared_ptr<NumericAddressedLowLevelTransferElement> _rawAccessor; // TODO needed???  // need a mixture of the two: _rawAccessor and _dev.
-    // in a numeric address backend, this is doing the actual transfer. not needed!
-    // this is supposed to be whats implementing doreadtransfer and dowritetransfer.
+    // boost::shared_ptr<NumericAddressedLowLevelTransferElement> _rawAccessor; // TODO needed???  // need a mixture of
+    // the two: _rawAccessor and _dev.
+    //  in a numeric address backend, this is doing the actual transfer. not needed!
+    //  this is supposed to be whats implementing doreadtransfer and dowritetransfer.
 
     /** the backend to use for the actual hardware access */
     // boost::shared_ptr<NumericAddressedBackend> _dev; //Looks like this is what we're replacing with our backend.
-    boost::shared_ptr<CommandBasedBackend> _dev; 
+    boost::shared_ptr<CommandBasedBackend> _dev;
     // need a mixture of the two: _rawAccessor and _dev.
 
     std::vector<std::string> readTransferBuffer;
     std::string writeTransferBuffer;
 
-      /** Backend specific implementation of preRead(). preRead() will call this function, but it will make sure that
+    /** Backend specific implementation of preRead(). preRead() will call this function, but it will make sure that
      *  it gets called only once per transfer.
      *
      *  No actual communication may be done. Hence, no runtime_error exception may be thrown by this function. Also it
      *  must be acceptable to call this function while the device is closed or not functional (see isFunctional()) and
      *  no exception is thrown. */
-    //virtual void doPreRead(TransferType) {} //Martin: PreRead is empty. 
+    // virtual void doPreRead(TransferType) {} //Martin: PreRead is empty.
 
-        /** Backend specific implementation of postRead(). postRead() will call this function, but it will make sure that
+    /** Backend specific implementation of postRead(). postRead() will call this function, but it will make sure that
      *  it gets called only once per transfer.
      *
      *  No actual communication may be done. Hence, no runtime_error exception may be thrown by this function. Also it
@@ -421,40 +419,41 @@ class CommandBasedBackend : public DeviceBackendImpl {
      */
     void doPostRead(TransferType t, bool updateDataBuffer) override {
       // Martin: converts text response to number. Fill it into the user buffer. TODO
-      if(updateDataBuffer){
-        //take std::vector<std::string> readTransferBuffer;
-        //convert strings to numbers
-        //fill them into buffer2D
+      if(updateDataBuffer) {
+        // take std::vector<std::string> readTransferBuffer;
+        // convert strings to numbers
+        // fill them into buffer2D
       }
-    } 
+    }
 
-        /** Backend specific implementation of preWrite(). preWrite() will call this function, but it will make sure that
+    /** Backend specific implementation of preWrite(). preWrite() will call this function, but it will make sure that
      *  it gets called only once per transfer.
      *
      *  No actual communication may be done. Hence, no runtime_error exception may be thrown by this function. Also it
      *  must be acceptable to call this function while the device is closed or not functional (see isFunctional()) and
      *  no exception is thrown. */
-    virtual void doPreWrite(TransferType, VersionNumber) {} //Martin: Swapps user buffer with the buffer sent to hardware
-    //Takes data coming in, a number, put it into a string format. Here we prepare the write command that will get sent. TODO
+    virtual void doPreWrite(TransferType, VersionNumber) {
+    } // Martin: Swapps user buffer with the buffer sent to hardware
+    // Takes data coming in, a number, put it into a string format. Here we prepare the write command that will get sent. TODO
 
-        /** Backend specific implementation of postWrite(). postWrite() will call this function, but it will make sure that
+    /** Backend specific implementation of postWrite(). postWrite() will call this function, but it will make sure that
      *  it gets called only once per transfer.
      *
      *  No actual communication may be done. Hence, no runtime_error exception may be thrown by this function. Also it
      *  must be acceptable to call this function while the device is closed or not functional (see isFunctional()) and
      *  no exception is thrown. */
-    //virtual void doPostWrite(TransferType, VersionNumber) {} //Martin: this is empty for us.
+    // virtual void doPostWrite(TransferType, VersionNumber) {} //Martin: this is empty for us.
 
-        /**
+    /**
      *  Implementation version of writeTransfer(). This function must be implemented by the backend. For the
      *  functional description read the documentation of writeTransfer().
      *
      *  Implementation notes:
      *  - Decorators must delegate the call to writeTransfer() of the decorated target.
      */
-    virtual bool doWriteTransfer(ChimeraTK::VersionNumber versionNumber) = 0; //actual write call to the backend
+    virtual bool doWriteTransfer(ChimeraTK::VersionNumber versionNumber) = 0; // actual write call to the backend
 
-/**
+    /**
      *  Implementation version of readTransfer() for synchronous reads. This function must be implemented by the
      * backend. For the functional description read the documentation of readTransfer().
      *
@@ -465,7 +464,7 @@ class CommandBasedBackend : public DeviceBackendImpl {
      *  - Delegations within the same object should go to the "do" version, e.g. to
      * BaseClass::doReadTransferSynchronously()
      */
-    virtual void doReadTransferSynchronously() = 0; //actual read call to backend.
+    virtual void doReadTransferSynchronously() = 0; // actual read call to backend.
   };
 
 } // namespace ChimeraTK
