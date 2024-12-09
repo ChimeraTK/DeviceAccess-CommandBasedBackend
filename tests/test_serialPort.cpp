dfa5f4c2cb5e84dac53346cd7caa67558df34ed7
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2024-12-09 10:44:31 +0100

    doc: Has doc strings
    doc strings imported from pre-refactored version.

--- tests/test_serialPort.cpp
+++ tests/test_serialPort.cpp
@@ -17,6 +17,8 @@ struct SerialCommandHandlerFixture {
   SerialCommandHandlerFixture() : s(dummy.deviceNode) {}
 };
 
+// Declares and registers a fixture used by all test cases under a test suite.
+// Each test case in the subtree of the test suite uses the fixture.
 BOOST_FIXTURE_TEST_SUITE(SerialCommandHandlerTests, SerialCommandHandlerFixture)
 
 BOOST_AUTO_TEST_CASE(testSimpleReply) {
@@ -35,6 +37,8 @@ BOOST_AUTO_TEST_CASE(testBasicCommand) {
 }
 
 BOOST_AUTO_TEST_CASE(testLongCommandBuffer) {
+  // Here the cmd string is longer than 256 character read buffer, forcing a multiple reads.
+  // cmd is 255 char long, sending 257 chars
   std::string cmd =
       "test2aaafirst100aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb2ndHund"
       "redbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccc201thru249cccccc"
@@ -44,12 +48,16 @@ BOOST_AUTO_TEST_CASE(testLongCommandBuffer) {
 }
 
 BOOST_AUTO_TEST_CASE(testLongCommandBufferFollowup) {
+  // Intermediate basic tests to see if the responce from the previous test overflows into the next reply.
   std::string cmd = "test3";
   std::string res = s.sendCommand(cmd);
   BOOST_TEST(res == cmd, "Failed long command buffer followup test");
 }
 
 BOOST_AUTO_TEST_CASE(testDelimiterStraddleBuffer) {
+  // cmd is 254 char long, so this sends 256 chars. The read buffer is 256 characters, so the repply comes in over two
+  // reads. res has 2-character delimiter straddle read buffer, so the first char of the delimiter comes into the buffer
+  // upon the first read, and second delimter char comes into the buffer upon the second read
   std::string cmd =
       "test4aaafirst100aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb2ndHund"
       "redbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccc201thru249cccccc"
@@ -65,6 +73,8 @@ BOOST_AUTO_TEST_CASE(testDelimiterStraddleBufferFollowup) {
 }
 
 BOOST_AUTO_TEST_CASE(testCommandExactBufferSize) {
+  // cmd is 253 char long, sending 255 chars
+  // So the command, together with delimiter, is exactly the size of the buffer.
   std::string cmd =
       "test6aaafirst100aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb2ndHund"
       "redbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccc201thru249cccccc"
